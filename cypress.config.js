d9effaf7dbbdff4dd0077ae162e04003b6d659dd
Arnaud Pinatel <tintin0013@gmail.com>
2026-02-19 13:30:21 +0100

    chore: point stable avant implémentation multi-pages et Cypress

---
+++ cypress.config.js
@@ -0,0 +1,12 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    baseUrl: "http://localhost:3000",   // URL locale de ton app React
+    specPattern: "cypress/e2e/**/*.cy.{js,jsx,ts,tsx}",  // Où sont tes tests e2e
+    supportFile: "cypress/support/e2e.js",  // fichier support
+    setupNodeEvents(on, config) {
+      // implémentation des événements node si besoin
+    },
+  },
+});
