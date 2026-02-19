c0d83901a9d7cf9ebac13c02d84fda3bd4fc6bee
Arnaud Pinatel <tintin0013@gmail.com>
2026-02-19 21:28:01 +0100

    Fix cypress.config.js conflit de merge

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,6 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-<<<<<<< HEAD
     baseUrl: "http://localhost:3000",   // URL locale de ton app React
     specPattern: "cypress/e2e/**/*.cy.{js,jsx,ts,tsx}",  // Où sont tes tests e2e
     supportFile: "cypress/support/e2e.js",  // fichier support
@@ -11,10 +10,3 @@ module.exports = defineConfig({
     },
   },
 });
-=======
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
-  },
-});
->>>>>>> revert_stable
